1a5e89adaa53a5e81b59809136413e40dfdc6fe7
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-30 21:58:55 +0800

    Tambah dan hapus

---
+++ rekmed/ngedit.php
@@ -0,0 +1,8 @@
+<?php
+
+	$id_obat = $_POST['id_obat'];
+	$nama_obat = $_POST['nama_obat'];
+	$ket_obat = $_POST['ket_obat'];
+
+	$connect->query("UPDATE tb_obat SET nama_obat='".$nama_obat."', ket_obat='".$ket_obat."' WHERE id_obat=".$id_obat.");
+?>
