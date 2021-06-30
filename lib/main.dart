1ac6bcebf5d26fae036f44b167d8c2e60405c71c
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-30 14:32:30 +0800

    mengubah import

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import './Detail.dart';
-import './adddata.dart';
+import './detail.dart';
+import './add.dart';
 
 void main() {
   runApp(new MaterialApp(
