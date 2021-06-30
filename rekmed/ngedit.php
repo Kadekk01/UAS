e1aeae29e8f76dede0f746f9746d2e362f1540f3
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-30 22:01:09 +0800

    ngedit lagi

--- rekmed/ngedit.php
+++ rekmed/ngedit.php
@@ -4,5 +4,5 @@
 	$nama_obat = $_POST['nama_obat'];
 	$ket_obat = $_POST['ket_obat'];
 
-	$connect->query("UPDATE tb_obat SET nama_obat='".$nama_obat."', ket_obat='".$ket_obat."' WHERE id_obat=".$id_obat.");
+	$connect->query("UPDATE tb_obat SET nama_obat='".$nama_obat."', ket_obat='".$ket_obat."' WHERE id_obat=".$id_obat);
 ?>
